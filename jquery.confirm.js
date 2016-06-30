c395df009430bbc20cffe5d2c8db25b931bed6d7
Sascha Weidner <support@sioweb.de>
2016-06-30 11:04:04 +0200

    Update jquery.confirm.js
    Custom Buttons

--- jquery.confirm.js
+++ jquery.confirm.js
@@ -7,7 +7,6 @@
       standardOptions = {
 	      debug: true,
 	      enabled: true,
-	      loadImagesFirst: true,
 	      container: window,
         template: null,
         title: 'Sind Sie sicher?',
@@ -15,6 +14,7 @@
         button_accept: 'OK',
         button_abort: 'Abbrechen',
         on: 'click',
+        custom_buttons: null,
         button_pressed: function(){},
         close: function(){},
         defaultAction: function(){},
@@ -28,17 +28,26 @@
         img = null;
     this.item = false;
     this.modal = null;
+    this.isHTML = false;
 
     this.initOptions = new Object(standardOptions);
     
     this.init = function(elem) {
+      var reload = arguments[1]||false;
       selfObj = this;
+
+      if(reload) {
+        selfObj.modal.remove();
+        selfObj.template = null;
+      }
 
       if(!this.container)
         this.container = window;
+      this.container = $(this.container);
+      
       this.elem = elem;
       this.item = $(this.elem);
-      this.container = $(this.container);
+      this.isHTML = selfObj.item[0].tagName.toLowerCase() === 'html';
 
       if(!selfObj.enabled)
         return;
@@ -47,8 +56,11 @@
 
       selfObj.modal = $(selfObj.template).appendTo('body');
 
-      selfObj.modal.click(function(e){
+      selfObj.modal.unbind('click').click(function(e){
         e.preventDefault();
+        e.stopPropagation();
+      });
+      selfObj.modal.find('a').unbind('click').click(function(e){
         e.stopPropagation();
       });
       selfObj.modal.find('.ui-modal-button').click(function(e){
@@ -76,14 +88,22 @@
 
     this.createTemplate = function() {
       if(!selfObj.template) {
-        selfObj.template = '<div class="ui-modal">';
+        selfObj.template = '<div class="ui-modal modal">';
           selfObj.template += '<div class="ui-modal-close"></div>';
-          selfObj.template += '<div class="ui-modal-inner">';
+          selfObj.template += '<div class="ui-modal-inner inner">';
             selfObj.template += '<h2>'+selfObj.title+'</h2>';
             selfObj.template += '<p>'+selfObj.content+'</p>';
             selfObj.template += '<div class="ui-modal-buttons">'
-              selfObj.template += '<div class="ui-modal-button ui-modal-ok">'+selfObj.button_accept+'</div>'
-              selfObj.template += '<div class="ui-modal-button ui-modal-abort">'+selfObj.button_abort+'</div>'
+              if(selfObj.button_accept) selfObj.template += '<div class="ui-modal-button ui-modal-ok">'+selfObj.button_accept+'</div>'
+              if(selfObj.button_abort) selfObj.template += '<div class="ui-modal-button ui-modal-abort">'+selfObj.button_abort+'</div>'
+              if(selfObj.custom_buttons)
+                for(var button in selfObj.custom_buttons) {
+                  selfObj.template += '<div class="ui-modal-button-custom ui-modal-custom-'+button+'">';
+                    if(selfObj.custom_buttons[button].link !== undefined) selfObj.template += '<a href="'+selfObj.custom_buttons[button].link+'"'+(selfObj.custom_buttons[button].target?' target="'+selfObj.custom_buttons[button].target+'"':'')+'>';
+                    selfObj.template += selfObj.custom_buttons[button].title;
+                    if(selfObj.custom_buttons[button].link !== undefined) selfObj.template += '</a>';
+                  selfObj.template += '</div>'
+                }
             selfObj.template += '</div>';
           selfObj.template += '</div>';
         selfObj.template += '</div>';
@@ -103,13 +123,18 @@
       if(!selfObj.enabled)
         return;
 
-      selfObj.item.unbind(selfObj.on).bind(selfObj.on,function(e){
-        e.preventDefault();
-        selfObj.defaultAction(e,this);
+      if(!this.isHTML) {
+        selfObj.item.unbind(selfObj.on).bind(selfObj.on,function(e){
+          e.preventDefault();
+          selfObj.defaultAction(e,this);
 
+          $('.ui-modal').removeClass('open');
+          selfObj.modal.addClass('open');
+        });
+      } else {
         $('.ui-modal').removeClass('open');
         selfObj.modal.addClass('open');
-      });
+      }
     };
   };
 
@@ -119,14 +144,13 @@
         returnElement = [];
         
     returnElement[0] = element.each(function(k,i) {
-      var pluginClass = $.data(this, pluginName),
-          args = Array.prototype.slice.call(arguments);
+      var pluginClass = $.data(this, pluginName);
 
       if(!settings || typeof settings === 'object' || settings === 'init') {
 
         if(!pluginClass) {
           if(settings === 'init')
-            settings = args[1] || {};
+            settings = arguments[1] || {};
           pluginClass = new PluginClass();
 
           var newOptions = new Object(pluginClass.initOptions);
@@ -138,7 +162,8 @@
           pluginClass.init(this);
           $.data(this, pluginName, pluginClass);
         } else {
-          return;
+          pluginClass.init(this,1);
+          $.data(this, pluginName, pluginClass);
         }
       } else if(!pluginClass) {
         return;
